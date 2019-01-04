431fde5db32fad5c421133892e235cf5d610edb5
Sebastián Contreras <s.contreras09@ufromail.cl>
2019-01-04 09:34:27 -0300

    3

--- src/main/java/Data/Lector.java
+++ src/main/java/Data/Lector.java
@@ -19,13 +19,41 @@ public class Lector {
         String country;
         String description;
         String price;
-        
+        Vino vino;
         ArrayList<Vino> cava = new ArrayList<>();
         
         String path = "datos-vino.csv";
         BufferedReader br = null;
         String line = "";
-        String datSplitBy = ";";
+        String datSplitBy = ",";
+        
+        try {
+            br = new BufferedReader(new FileReader(path));
+            int first=1;
+            while ((line = br.readLine()) != null) {
+                String[] auxVino = line.split(datSplitBy);
+                country = auxVino[1].replace("NULL", "NODATA");
+                description = auxVino[2].replace("NULL", "NODATA");
+                price = auxVino[5].replace("NULL", "0.0");
+                if (!(first==1)){
+                    vino = new Vino(country, description, Double.parseDouble(price));
+                    cava.add(vino);
+                }
+                first = 0;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         
         return cava;
     }

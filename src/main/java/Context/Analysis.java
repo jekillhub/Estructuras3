4c259f03c205c2369d626d62d3ae8cbcccb7aa02
jekill <1jorge.echeverria@gmail.com>
2019-01-04 09:56:06 -0300

    mostrar info mayores 500

--- src/main/java/Context/Analysis.java
+++ src/main/java/Context/Analysis.java
@@ -1,32 +1,34 @@
 package Context;
 
 import java.util.ArrayList;
+
 /**
- *
  * @author Jekill
  */
 public class Analysis {
 
-    public static ArrayList<Vino> mayores500(ArrayList<Vino> cava){
+    public static ArrayList<Vino> mayores500(ArrayList<Vino> cava) {
         ArrayList<Vino> mayores = new ArrayList<Vino>();
 
-        for (int i=0; i<cava.size(); i++){
-            if(cava.get(i).getPrice() > 500){
+        for (int i = 0; i < cava.size(); i++) {
+            if (cava.get(i).getPrice() > 500) {
                 mayores.add(cava.get(i));
             }
         }
         return mayores;
     }
 
-    public static ArrayList<String> paisOrigen(ArrayList<Vino> mayores){
-        ArrayList<String> paises = new ArrayList<String>();
-
-        for (int i=0; i<mayores.size(); i++){
-            paises.add(mayores.get(i).getCountry());
+    public static void mostrarMayores500(ArrayList<Vino> mayores) {
+        for (int i = 0; i < mayores.size(); i++) {
+            System.out.println("Nombre: " + mayores.get(i).getTitle() + "\t" + "Precio: " + mayores.get(i).getPrice());
         }
-        return paises;
     }
 
+    public static void mostrarPaises(ArrayList<Vino> mayores) {
+        for (int i = 0; i < mayores.size(); i++) {
+            System.out.println("Nombre: " + mayores.get(i).getTitle() + "\t" + "País: " + mayores.get(i).getCountry());
+        }
+    }
 
 
 }

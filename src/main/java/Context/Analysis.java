5421bda14d053e1bd848ad4a4aebabdaa46ccf96
jekill <1jorge.echeverria@gmail.com>
2019-01-04 09:35:49 -0300

    mayores 500

---
+++ src/main/java/Context/Analysis.java
@@ -0,0 +1,20 @@
+package Context;
+
+/**
+ *
+ * @author Jekill
+ */
+public class Analysis {
+
+    public static ArrayList<Vino> mayores500(ArrayList<Vino> cava){
+        ArrayList<Vino> mayores = new ArrayList<Vino>();
+
+        for (int i=0; i<cava.size(); i++){
+            if(cava.get(i).getPrice() > 500){
+                mayores.add(cava.get(i));
+            }
+        }
+        return mayores;
+    }
+
+}
